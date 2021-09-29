7509c0ea6ac3da6f3bafae3a0db5ded8493de439
cjgamble21 <cjgamble21@gmail.com>
2021-09-29 15:18:13 -0500

    Added Catch2 tests through CMake FetchContent

---
+++ Tests/AlgoVector_tests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Connor Gamble on 9/29/21.
+//
+
+#include <catch2/catch_all.hpp>
+#include "../Containers/AlgoVector.h"
+
+TEST_CASE("A vector object can be created") {
+    algo::AlgoVector<std::string> vector;
+}
